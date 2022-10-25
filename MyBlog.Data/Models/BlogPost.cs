8b9be8d9479b7825f9d52029d9afa762fd3504f7
Rob Garner <rgarner011235@gmail.com>
2022-10-24 19:36:07 -0600

    Fixed foreign key issue

--- MyBlog.Data/Models/BlogPost.cs
+++ MyBlog.Data/Models/BlogPost.cs
@@ -6,9 +6,9 @@ namespace MyBlog.Data.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Text { get; set; }
-        public DateTime PublishDate { get; set; }
-        public Category Category { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+        public string? Text { get; set; }
+        public DateTime? PublishDate { get; set; }
+        public Category? Category { get; set; }
+        public ICollection<Tag>? Tags { get; set; }
     }
 }
