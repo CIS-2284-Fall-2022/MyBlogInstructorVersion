b973d181970e0276385eae214d78f8e01b7b59a9
Rob Garner <rgarner011235@gmail.com>
2022-11-07 18:25:35 -0700

    Added HttpClientExtension class

---
+++ MyBlog.Data/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> DeleteAsJsonAsync<T>(this HttpClient httpClient, string requestUri, T data)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, requestUri);
+            request.Content = Serialize(data);
+            return httpClient.SendAsync(request);
+        }
+
+        public static Task<HttpResponseMessage> DeleteAsJsonAsync<T>(this HttpClient httpClient, string requestUri, T data, CancellationToken cancellationToken)
+        {
+            return httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, requestUri) { Content = Serialize(data) }, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> DeleteAsJsonAsync<T>(this HttpClient httpClient, Uri requestUri, T data)
+        {
+            return httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, requestUri) { Content = Serialize(data) });
+        }
+
+        public static Task<HttpResponseMessage> DeleteAsJsonAsync<T>(this HttpClient httpClient, Uri requestUri, T data, CancellationToken cancellationToken)
+        {
+            return httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, requestUri) { Content = Serialize(data) }, cancellationToken);
+        }
+
+        private static HttpContent Serialize(object data)
+        {
+            return new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+        }
+    }
+}
