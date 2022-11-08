c6572bca3d91cb5f2bf0d9b5715d9df8faf177d9
Rob Garner <rgarner011235@gmail.com>
2022-11-07 18:58:20 -0700

    Added auth to DbContext

--- MyBlog.Data/MyBlogDbContext.cs
+++ MyBlog.Data/MyBlogDbContext.cs
@@ -1,23 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
 using MyBlog.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyBlog.Data
 {
-    public class MyBlogDbContext:DbContext
+    public class MyBlogDbContext:ApiAuthorizationDbContext<AppUser>
     {
-        public MyBlogDbContext(DbContextOptions context):base(context)
+        public MyBlogDbContext(DbContextOptions options):base(options,new OperationalStoreOptionsMigrations())
         {            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
 
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
+    }
+
+    internal class OperationalStoreOptionsMigrations : IOptions<OperationalStoreOptions>
+    {
+        public OperationalStoreOptions Value => new OperationalStoreOptions()
+        {
+            DeviceFlowCodes = new TableConfiguration("DeviceCodes"),
+            EnableTokenCleanup = false,
+            PersistedGrants = new TableConfiguration("PersistedGrants"),
+            TokenCleanupBatchSize = 100,
+            TokenCleanupInterval = 3600,
+        };
     }
 
     public class MyBlogDbContextFactory : IDesignTimeDbContextFactory<MyBlogDbContext>
