9e47c45520036bb3662d8d0968e39b39160b77db
rob011235 <rgarner011235@gmail.com>
2022-11-13 20:28:38 -0700

    Attempts to fix cascading add issues

--- MyBlog.Data/Models/BlogPost.cs
+++ MyBlog.Data/Models/BlogPost.cs
@@ -8,11 +8,11 @@ namespace MyBlog.Data.Models
         public int Id { get; set; }
         [Required]
         [MinLength(5)]
-        public string Title { get; set; }
+        public string Title { get; set; } = "";
         [Required]
         public string Text { get; set; } = "";
-        public DateTime PublishDate { get; set; }
-        public Category Category { get; set; }
+        public DateTime PublishDate { get; set; }=DateTime.Now;
+        public Category? Category { get; set; }
         public ICollection<Tag> Tags { get; set; } = new List<Tag>();
 
     }
--- MyBlog.Data/Repos/BlogPostRepo.cs
+++ MyBlog.Data/Repos/BlogPostRepo.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyBlog.Data.Interfaces;
 using MyBlog.Data.Models;
+using System.Reflection.Metadata;
 
 namespace MyBlog.Data.Repos
 {
@@ -58,19 +59,22 @@ namespace MyBlog.Data.Repos
                 post.Tags = context.Tags.Where(t => ids.Contains(t.Id)).ToList();
                 //Fix reference to blog post
                 post.Category = await context.Categories.FirstOrDefaultAsync(c => c.Id == post.Category.Id);
-                context.Entry(post).State = EntityState.Modified;       
-
-                //Code from text book:
-                //var currentpost = await context.BlogPosts.Include(p => p.Category).Include(p => p.Tags).FirstOrDefaultAsync(p => p.Id == post.Id);
-                //currentpost.PublishDate = post.PublishDate;
-                //currentpost.Title = post.Title;
-                //currentpost.Text = post.Text;
-                //var ids = post.Tags.Select(t => t.Id);
-                //currentpost.Tags = context.Tags.Where(t => ids.Contains(t.Id)).ToList();
-                //currentpost.Category = await context.Categories.FirstOrDefaultAsync(c => c.Id == post.Category.Id);
+                context.Entry(post).State = EntityState.Modified;
             }
+              //  Code from text book:
+            //var currentpost = await context.BlogPosts.Include(p => p.Category).Include(p => p.Tags).FirstOrDefaultAsync(p => p.Id == post.Id);
+            //    currentpost.PublishDate = post.PublishDate;
+            //    currentpost.Title = post.Title;
+            //    currentpost.Text = post.Text;
+            //    var ids = post.Tags.Select(t => t.Id);
+            //    currentpost.Tags = context.Tags.Where(t => ids.Contains(t.Id)).ToList();
+            //    currentpost.Category = await context.Categories.FirstOrDefaultAsync(c => c.Id == post.Category.Id);
+            //}
             await context.SaveChangesAsync();
             return post;
         }
+
+
+
     }
 }
